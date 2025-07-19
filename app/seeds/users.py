a45c6a382aa418ec3a655e7263b5fa05e52b7655
Crios223 <crios2231993@gmail.com>
2025-07-18 20:53:45 -0400

    Migration fix rerun flask migrations

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,18 +3,74 @@ from sqlalchemy.sql import text
 
 
 # Adds a demo user, you can add other users here if you want
-def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+# def seed_users():
+#     demo = User(
+#         username='chris',
+#         email='chris@chris.com',
+#         password='password',
+#         first_name='Chris',
+#         last_name='Rios',
+#         profile_image_url='https://example.com/demo.jpg'  # optional
+#     )
+#     marnie = User(
+#         username='jean',
+#         email='jean@jean.com',
+#         password='password',
+#         first_name='Jean',
+#         last_name='Casanova',
+#         profile_image_url='https://example.com/marnie.jpg'
+#     )
+#     bobbie = User(
+#         username='jose',
+#         email='jose@jose.com',
+#         password='password',
+#         first_name='Jose',
+#         last_name='Garcia',
+#         profile_image_url='https://example.com/bobbie.jpg'
+#     )
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+#     db.session.add_all([demo, marnie, bobbie])
+#     db.session.commit()
+
+
+def seed_users():
+    chris = User(
+        username='chris',
+        email='chris@chris.com',
+        password='password',
+        first_name='Chris',
+        last_name='Rios',
+        profile_image_url='https://example.com/demo.jpg'
+    )
+    jean = User(
+        username='jean',
+        email='jean@jean.com',
+        password='password',
+        first_name='Jean',
+        last_name='Casanova',
+        profile_image_url='https://example.com/marnie.jpg'
+    )
+    jose = User(
+        username='jose',
+        email='jose@jose.com',
+        password='password',
+        first_name='Jose',
+        last_name='Garcia',
+        profile_image_url='https://example.com/bobbie.jpg'
+    )
+    east = User(
+        username='east',
+        email='east@east.com',
+        password='password',
+        first_name='East',
+        last_name='Allen',
+        profile_image_url='https://example.com/east.jpg'
+    )
+
+    db.session.add_all([chris, jean, jose, east])
     db.session.commit()
+
+
 
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
