a722e988114b3c4efe4113326d2e0601c7fabbd2
Your Name <josegarciaemail96@gmail.com>
2025-07-22 22:40:00 -0400

    add review routes and form setup

---
+++ app/forms/review_form.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField, validators
+from wtforms.validators import DataRequired, Length, NumberRange
+
+class ReviewForm(FlaskForm):
+    rating = IntegerField(
+        'Rating',
+        validators=[
+            DataRequired(message='Rating is required.'),
+            NumberRange(min=1, max=5, message='Rating must be between 1 and 5.')
+        ])
+    title = StringField('Title',validators=[
+        DataRequired(message='Title is required.'),
+        Length(max=100, message='Title must be under 100 characters long.')
+    ])
+    content = TextAreaField('Content', validators=[
+        DataRequired(message='Content is required.'),
+        Length(max=500, message='Content must be under 500 characters long.')
+    ]
+    )
