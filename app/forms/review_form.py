a646361ce3dcccc5910550986359ba5f63cfdf78
Your Name <josegarciaemail96@gmail.com>
2025-07-22 23:05:24 -0400

    updating review form

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, validators
+from wtforms import StringField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired, Length, NumberRange
 
 class ReviewForm(FlaskForm):
@@ -9,12 +9,15 @@ class ReviewForm(FlaskForm):
             DataRequired(message='Rating is required.'),
             NumberRange(min=1, max=5, message='Rating must be between 1 and 5.')
         ])
-    title = StringField('Title',validators=[
-        DataRequired(message='Title is required.'),
-        Length(max=100, message='Title must be under 100 characters long.')
-    ])
-    content = TextAreaField('Content', validators=[
-        DataRequired(message='Content is required.'),
-        Length(max=500, message='Content must be under 500 characters long.')
-    ]
-    )
+    title = StringField(
+        'Title',
+        validators=[
+            DataRequired(message='Title is required.'),
+            Length(max=100, message='Title must be under 100 characters long.')
+        ])
+    content = TextAreaField(
+        'Content',
+        validators=[
+            DataRequired(message='Content is required.'),
+            Length(max=500, message='Content must be under 500 characters long.')
+        ])
