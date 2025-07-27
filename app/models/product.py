a4023444428496b4df707226f7472a8bebf7b320
Crios223 <crios2231993@gmail.com>
2025-07-26 21:24:54 -0400

    OG Notes

--- app/models/product.py
+++ app/models/product.py
@@ -1,33 +1,3 @@
-#OG CODE
-
-# from .db import db, environment, SCHEMA, add_prefix_for_prod
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from .db import db, environment, SCHEMA
-# from datetime import datetime
-
-# class Product(db.Model):
-#     __tablename__ = 'products'
-
-#     if environment == "production":
-#         __table_args__ = {'schema': SCHEMA}
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     seller_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
-#     title = db.Column(db.String(100), nullable=False)
-#     description = db.Column(db.Text, nullable=False)
-#     price = db.Column(db.Numeric(10, 2), nullable=False)
-#     cover_image_url = db.Column(db.String(255))
-#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-#     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-#     seller = db.relationship("User", back_populates="products")
-#     reviews = db.relationship("Review", back_populates="product", cascade="all, delete-orphan")
-#     cart_items = db.relationship("ShoppingCart", back_populates="product", cascade="all, delete-orphan")
-#     wish_list_items = db.relationship("Wishlist", back_populates="product", cascade="all, delete-orphan")
-
-
-
-
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from .db import db, environment, SCHEMA
