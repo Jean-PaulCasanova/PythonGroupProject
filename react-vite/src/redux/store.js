124ad9ec55ae8d12bf0df74570b7de880a40a8de
jean-paulcasanova <japes365@gmail.com>
2025-07-26 20:03:54 -0400

    auth wishlist set up and redux

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -6,9 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import wishlistReducer from "./wishlist"
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  wishlist: wishlistReducer,
 });
 
 let enhancer;
