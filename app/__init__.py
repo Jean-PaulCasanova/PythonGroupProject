6ef9b859bb8281ede5fa54dbc79a5c24f191119b
jean-paulcasanova <japes365@gmail.com>
2025-07-31 20:20:30 -0400

    still debugging app init

--- app/__init__.py
+++ app/__init__.py
@@ -389,6 +389,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.product_routes import product_routes
+from .api.wishlist_routes import wishlist_routes
 from .seeds import seed_commands
 from .config import Config
 
